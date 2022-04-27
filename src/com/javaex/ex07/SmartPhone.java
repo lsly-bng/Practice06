aa9103b2bb605af623737e6ca9a3b44cd9e3ac3b
bhw <wyhoys92@gmail.com>
2022-04-27 20:19:07 +0900

    Practice06 updated_02

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -4,12 +4,21 @@ public class SmartPhone extends MusicPhone {
 
 	public void execute(String str) {
 
+		/*
+		 if ("앱".equals(str)) { 
+		 		openApp(); 
+		 	} else if ("통화".equals(str)) {
+		 		super.execute(str); 
+		 	} else if ("음악".equals(str)) { 
+		 		playMusic(); 
+		 	}
+		 }  //기존 코딩 
+		 */
+		
 		if ("앱".equals(str)) {
 			openApp();
-		} else if ("통화".equals(str)) {
-			super.execute(str);
-		} else if ("음악".equals(str)) {
-			playMusic();
+		} else {
+			super.execute(str); //참조 코딩 : 이렇게만해도 부모 내용 상속되며, playMusic도 Override 적용되어 원하는 출력값 나옴
 		}
 	}
 
